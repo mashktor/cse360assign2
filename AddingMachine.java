a274e4260af82fadaf98bd0aab2c2d2fe1bf9afe
Miras Ashktorab <mashktor@asu.edu>
2019-10-09 05:26:47 -0700

    Initial commit: Javadoc created for AddingMachine

---
+++ AddingMachine.java
@@ -0,0 +1,69 @@
+/**
+ * @author:         Miras Ashktorab
+ * @date:           10/09/2019
+ * @class ID:       70641
+ * @Assignment:     Assignment 2    (package name: cse360assign2)
+ *
+ * @Description:    This assignment provides insight into version control (GitHub)
+ * 					with practice to use git commands such as commit, add, status.
+ */
+
+package cse360assign2;
+
+
+/**
+ * Class AddingMachine is a very simple calculator.
+ */
+public class AddingMachine {
+
+	/**
+	 * Total holds the total calculated value
+	 */
+	private int total;
+
+	/**
+	 * NOT NEEEDED, initializes the total variable
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+	 * This method gets total
+	 * @return the total amount
+	 */
+	public int getTotal () {
+		return 0;
+	}
+
+	/**
+	 * This Method adds a value to the total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+
+	/**
+	 * This Method subtracts a value from the total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+
+	/**
+	 * This Method converts the calculation instructions to a String
+	 * @return String of calculation instructions
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * This Method clears the total variable
+	 */
+	public void clear() {
+	
+	}
+}
